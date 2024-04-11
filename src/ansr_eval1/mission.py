27236ed7d4871e1bdcbe64f0b321051fd1ed69d0
cong-liu-2000 <cong.liu@collins.com>
2024-04-11 12:08:16 -0500

    Convert to ENU coordinates

--- src/ansr_eval1/mission.py
+++ src/ansr_eval1/mission.py
@@ -9,6 +9,7 @@ class Car:
         self.id = id
         self.priority = priority
         self.map = None
+        self.detected = False
 
 class Region:
 
@@ -29,12 +30,29 @@ class Cell:
     def __init__(self, polygon):
         self.polygon = polygon
         self.in_keep_out_zone = False
+        self.in_any_region = False
+        self.visited = False
+
+class Route:
+
+    def __init__(self, points_list, rectangles):
+        self.points_list = points_list
+        self.rectangles = rectangles
 
 def carSortCriterion(car):
 	return car.priority
 
 def polygonSortCriterion(polygon):
 	return polygon.probability
+
+def cellsInKeepOutZone(cells, zone):
+        cell_list = []
+        cell_idx = 0
+        for cell in cells:
+            if cell.polygon.intersects(zone.polygon):
+                cell_list.append(cell_idx)
+            cell_idx = cell_idx + 1
+        return cell_list
 
 def cellInKeepOutZone(cell, zone):
         return (cell.polygon.intersects(zone.polygon))
@@ -123,7 +141,11 @@ class Mission:
             region_list = []
             for map in entity["entity_priors"]["location_belief_map"]:
                 for polygon in map["polygon_vertices"]:
-                    poly = Polygon(polygon)
+                    enu_points = []
+                    for point in polygon:
+                        enu_point = point[::-1]
+                        enu_points.append(enu_point)
+                    poly = Polygon(enu_points)
                     prob = map["probability"]
                     region = Region(prob, poly)
                     region_list.append(region)
@@ -134,22 +156,41 @@ class Mission:
 
         self.car_list.sort(key=carSortCriterion)
 
-        for zone in self.description_data["scenario_constraints"]["spatial_constraints"]["keep_out_zones"]:
-            polygon = zone["keep_out_polygon_vertices"]
-            poly = Polygon(polygon)
-            t_start = zone["no_earlier_than"]
-            t_end = zone["no_later_than"]
+        for keep_out_zone in self.description_data["scenario_constraints"]["spatial_constraints"]["keep_out_zones"]:
+            enu_points = []
+            for point in  keep_out_zone["keep_out_polygon_vertices"]:
+                enu_point = point[::-1]
+                enu_points.append(enu_point)
+            poly = Polygon(enu_points)
+            t_start = keep_out_zone["no_earlier_than"]
+            t_end = keep_out_zone["no_later_than"]
             zone = Zone(poly, t_start, t_end)
+            cells = cellsInKeepOutZone(self.cells, zone)
+            zone.cells = cells
             self.keep_out_zones.append(zone)
 
 
-        for route in self.description_data["scenario_objective"]["routes_of_interest"]:
-            for route_points in route["route_points"]:
-                self.route_list.append(route_points)
+        for route_data in self.description_data["scenario_objective"]["routes_of_interest"]:
+            points_list = []
+            for route_points in route_data["route_points"]:
+                enu_points = []
+                for point in route_points:
+                    enu_point = point[::-1]
+                    enu_points.append(enu_point)
+                points_list.append(enu_points)
 
-            for rectangle in route["enclosing_rectangles"]:
-                rect = Polygon(rectangle)
-                self.rect_list.append(rect)
+            rectangles = []
+            for rectangle in route_data["enclosing_rectangles"]:
+                enu_points = []
+                for point in rectangle:
+                    enu_point = point[::-1]
+                    enu_points.append(enu_point)
+                rect = Polygon(enu_points)
+                rectangles.append(rect)
+
+            route = Route(points_list, rectangles)
+            self.route_list.append(route)
+
 
         # Postprocessing
         for car in self.car_list:
@@ -160,6 +201,51 @@ class Mission:
         for cell in self.cells:
             if cellInAnyKeepOutZone(cell, self.keep_out_zones):
                 cell.in_keep_out_zone = True
+
+    def allCarsAreDetected(self):
+        for car in self.car_list:
+            if not car.detected:
+                return False
+        return True
+
+    def getSpecialAOIPoints(self):
+        points = ([64, 64], [-64, -64], [64, -64])
+        AOI_points = []
+        for car in self.car_list:
+            for region in car.map:
+                poly = region.polygon
+                for point in points:
+                    p = Point(point)
+                    if poly.contains(p) or poly.exterior.distance(p) < 20:
+                        if point not in AOI_points:
+                            AOI_points.append(point)
+        return AOI_points
+
+    def getSpecialAOIPointEntry(self, AOI_point):
+        x = AOI_point[0]
+        y = AOI_point[1]
+        dx_array = [64, -64]
+        dy_array = [64, -64]
+        points = []
+        points.append(AOI_point)
+        for dx in dx_array:
+            for dy in dy_array:
+                intersect = False
+                x1 = x + dx
+                y1 = y + dy
+                points.append([x1,y1])
+                print(points)
+                linestring = LineString(points)
+                for zone in self.keep_out_zones:
+                    if linestring.intersects(zone.polygon):
+                        intersect = True
+                        break
+                if intersect:
+                    points.pop()
+                    print(points)
+                else:
+                    return [x1, y1]
+        return [1e5, 1e5]
 
 if __name__ == "__main__":
         mission = Mission('../../../mission-schema/examples/Maneuver/RouteSearch/RSM002/description.json', '../../../mission-schema/examples/Maneuver/RouteSearch/RSM002/config.json') # change this to near the end Mission('../../../mission_briefing/description.json')
@@ -172,7 +258,7 @@ if __name__ == "__main__":
                 print(region.polygon)
                 print('cells: ', region.cells)
                 x,y = region.polygon.exterior.xy
-                #plt.plot(x,y)
+                plt.plot(x,y)
 
         for zone in mission.keep_out_zones:
             print("t_start ", zone.t_start)
@@ -183,13 +269,14 @@ if __name__ == "__main__":
         for cell in mission.cells:
             print(cell.in_keep_out_zone)
             x,y = cell.polygon.exterior.xy
-            # plt.plot(x,y)
+            plt.plot(x,y)
 
         for route in mission.route_list:
-            linestring = LineString(route)
-            plt.plot(*linestring.xy,  linewidth=7.0)
+            for points in route.points_list:
+                linestring = LineString(points)
+                plt.plot(*linestring.xy,  linewidth=7.0)
 
-        for rect in mission.rect_list:
+            for rect in route.rectangles:
                 x,y = rect.exterior.xy
                 plt.plot(x,y)
 
